be34c09514533c8b6db042782569487329141a3a
Sucharitha V <sucharitha93@gmail.com>
2015-09-08 19:57:48 +0530

    Latest Python UDP_Server code

--- connectors/chs-rpi-server/RpiServer/src/udp_serverv2.py
+++ connectors/chs-rpi-server/RpiServer/src/udp_serverv2.py
@@ -8,8 +8,14 @@ import time
 import commands
 import bp
 import json
-
-
+#function that creates a sensor object
+def sensorobject(sid,sname,stype):
+	ob=dict()
+	ob["sensorid"]=sid
+	ob["sensorname"]=sname
+	ob["sensortype"]=stype
+	return ob
+#function that determines which sensors are connected in which port(BP & Pulseoxy)
 def sensorconnected():
 	p={}
 	b={}
@@ -18,139 +24,120 @@ def sensorconnected():
 	port2 = '/dev/ttyUSB1'
 	bp=''
 	po=''
+	#command that lists the connected sensors
 	portlist=commands.getoutput("ls /dev/ttyUSB*")
+	#case if both ports are occupied by sensors
 	if 'USB0' in portlist and 'USB1' in portlist:
+		#case for identifying the corresponding port for each of the sensors
 		try:
 			pup.readsensor(1,port1)
 			po=port1
 			bp=port2
-			p['sensorid']=1
-			p['sensorname']='nonin'
-			p['sensortype']='medical'
-			b['sensorid']=2
-			b['sensorname']='bp'
-			b['sensortype']='medical'
+			p=sensorobject(1,'nonin','medical')
+			b=sensorobject(2,'bp','medical')
 			sensors.append(p)
 			sensors.append(b)
 		except ValueError:
 			po=port2
 			bp=port1
-			b['sensorid']=1
-			b['sensorname']='bp'
-			b['sensortype']='medical'
-			p['sensorid']=2
-			p['sensorname']='nonin'
-			p['sensortype']='medical'
+			b=sensorobject(1,'bp','medical')
+			p=sensorobject(2,'nonin','medical')
 			sensors.append(b)
 			sensors.append(p)
 		return po,bp,sensors
+	#case if only one sensor is connected and identifying which sensor is connected	
 	elif 'USB0' in portlist:
 		try:
 			pup.readsensor(1,port1)
 			po=port1
-			p['sensorid']=1
-			p['sensorname']='nonin'
-			p['sensortype']='medical'
+			p=sensorobject(1,'nonin','medical')
 			sensors.append(p)
 		except ValueError:
 			print "only BP sensor connected in PORT1"
 			bp=port1
-			b['sensorid']=1
-			b['sensorname']='bp'
-			b['sensortype']='medical'
+			b=sensorobject(1,'bp','medical')
 			sensors.append(b)
 		return po,bp,sensors
 	elif 'USB1' in portlist:
 		try:
 			pup.readsensor(1,port2)
 			po=port2
-			p['sensorid']=1
-			p['sensorname']='nonin'
-			p['sensortype']='medical'
+			p=sensorobject(1,'nonin','medical')
 			sensors.append(p)
 		except ValueError:
 			print "only BP sensor connected in PORT2"
 			bp=port2
-			b['sensorid']=1
-			b['sensorname']='bp'
-			b['sensortype']='medical'
+			b=sensorobject(1,'bp','medical')
 			sensors.append(b)
 		return po,bp,sensors
+	#case when no sensor is connected(the list of sensors is empty)
 	else:
 		print "Sensor not connected"
 		return po,bp,sensors
-
-
-
-#sensors=[{sensorid:1,sensorname:nonin,sensortype:medical}, {sensorid:2,sensorname:bp,sensortype:medical}]
+#function to get the mac address of the hardware
 def getmac(iface):
 	mac=commands.getoutput("ifconfig "+iface+" | grep HWaddr | awk '{ print $5 }'")
 	if len(mac)==17:
 		return mac
-
-
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 #Function to get the IP Address
 def get_ip_address(ifname,sock):
 	#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	return socket.inet_ntoa(fcntl.ioctl(sock.fileno(),0x8915,struct.pack('256s', ifname[:15]))[20:24])
-
-#TODO get ip from program
+#case to determine the OS platform
 if platform.system()=='Windows':
 	ip = socket.gethostbyname(socket.gethostname())
 else:
 	ip = get_ip_address('wlan0',sock)
 	mac= getmac('wlan0')
-
 # Bind the socket to the port
 server_address = (ip, 10000)
 sock.bind(server_address)
-
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-
+print >>sys.stderr, 'starting up on %s port %s' , server_address
+#function to get the current time 
 def gettime():
 	t = time.strftime("%x") + time.strftime("%X")
 	return t
-
 while True:
 	po,bp,sensors=sensorconnected()
         print >>sys.stderr, '\nwaiting to receive message'
         data, address = sock.recvfrom(4096)
-        
         print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
         print >>sys.stderr, data
-        
         if data:
                 recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+		message = dict()
                 #TODO find if the packet is a discover packet or request packet
                 #TODO pick self ip addresses automatically
                 #TODO pick remote ip addresses from json
                 #TODO extract port from message
                 #TODO populate sensors from a settings file
-		
-                if "discover" in data :
+	        if "discover" in data :
                         t = gettime()
                         server_address = (address[0], 10001)
-                        message = dict()
-                        message[""]=""
-                        message[""]=""
-                        message[""]=""
-                        message[""]=""
-                        message[""]=""
-			message = """{ type: "discover_reply", name:"rpi_board" , sdr:"""+str(ip)+"""  , rcv:"""+str(address[0])+""" , mac:"""+str(mac)+""", time:"""+t+""" , argv:"""+sensors+"""}"""
+                        #message to be sent created as a dictionary
+                        message["type"]="discover_reply"
+                        message["name"]="rpi_board"
+                        message["sdr"]=ip
+                        message["rcv"]=address[0]
+                        message["mac"]=mac
+                        message["time"]=t
+                        message["argv"]=sensors
+                        #dictionary converted to json
+                        jsonmessage=json.dumps(message)
                         try:
-                    # Send data
-                                print >>sys.stderr, 'sending "%s"' % message
-                                sent = recv_sock.sendto(message, server_address)
+                    	# Send data
+                                print >>sys.stderr, 'sending "%s"' , jsonmessage
+                                sent = recv_sock.sendto(jsonmessage, server_address)
 			finally:
                                 print >>sys.stderr, 'closing socket'
                                 recv_sock.close()
                 elif "request" in data:
+                	value=dict()
 			t = gettime()
 			readings=[]
+			#case to get the readings from the sensor
 			if "nonin" in data:
 				if po=='':
 					readings=[[-1,-1]]
@@ -168,12 +155,20 @@ while True:
                         #readings=pulseoxy.readsensor(1)
                         #readings = [[78,35]]
 			server_address = (address[0], 10002)
-                        message = """{type:request_reply, sdr:"""+str(ip)+"""  , rcv:"""+str(address[0])+""", time:"""+t+""" , argv: [ {"sensorid":"""+str(sensorname)+""", "readings":""" + str(readings)+ """ }] }"""
-
+			#message to be sent created as a dictionary
+			message["type"]="request_reply"
+                        message["sdr"]=ip
+                        message["rcv"]=address[0]
+                        message["time"]=t
+                        value["sensorname"]=sensorname
+                        value["readings"]=readings
+                        message["argv"]=value
+                        #dictionary converted to json
+                        jsonmessage=json.dumps(message)
                         try:
                                 # Send data
-                                print >>sys.stderr, 'sending "%s to %s "' % (message , server_address)
-                                sent = recv_sock.sendto(message, server_address)
+                                print >>sys.stderr, 'sending "%s to %s "' , (jsonmessage , server_address)
+                                sent = recv_sock.sendto(jsonmessage, server_address)
 		                        
                         finally:
                                 print >>sys.stderr, 'closing socket'
